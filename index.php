6e6a77c0f2c15641814d1bd6e9fcbf17d92573a9
AgustinTrevia <106684361+AgustinTrevia@users.noreply.github.com>
2022-07-19 16:00:11 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
 					</ul>
 
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li>&copy; Untitled. Derechos reservados.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
 					</ul>
 
 				</div>
